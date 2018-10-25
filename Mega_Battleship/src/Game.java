15701e247ba36497b7bc26a2e974da4acea16a83
Andrew Lewis <ajlewis1@me.com>
2018-10-25 14:49:17 -0600

    More progress on the server side.

---
+++ Mega_Battleship/src/Game.java
@@ -0,0 +1,24 @@
+public class Game {
+
+
+    public static void main(String[] args){
+
+        Game game = new Game();
+
+    }
+
+    public Boolean removeFromServer(Player player){
+
+        Boolean success = false;
+
+
+        return success;
+    }
+
+
+    // Two generic methods were written as follows in UML:
+    // get...():T
+    // set...(T): Boolean
+
+
+}
---
+++ Mega_Battleship/src/Ship.java
@@ -0,0 +1,26 @@
+public abstract class Ship {
+
+    private double _attack;
+    private double _health;
+    private double _resiliency;
+    private double _speed;
+    private double _accuracy;
+    private double _turning;
+
+    public void Ship(){
+
+    }
+
+    public void attack(){
+
+    }
+
+    public void moveTo(int x, int y){
+
+    }
+
+    // Two generic methods were written as follows in UML:
+    // get...():T
+    // set...(T): Boolean
+
+}
