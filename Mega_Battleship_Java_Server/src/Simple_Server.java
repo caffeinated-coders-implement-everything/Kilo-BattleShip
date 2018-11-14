4b902af012dc66b68619316a8463749bdea2687d
Andrew Lewis <ajlewis1@me.com>
2018-11-14 15:20:18 -0700

    Got client-side mostly built. Needs input from either the actual server or simulated server for testing.

---
+++ Mega_Battleship_Java_Server/src/Simple_Server.java
@@ -0,0 +1,36 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class SimpleServer {
+
+    public static void main(String[] args) {
+
+        try {
+
+
+            ServerSocket socketServer = new ServerSocket(9888, 5);
+            while (true) {
+
+                Socket connection = socketServer.accept();
+
+                PrintWriter output = new PrintWriter(connection.getOutputStream(), true);
+
+                BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                Thread.sleep(3000); //wait for 3 seconds (do processing.)
+
+                output.println("Connected to Java server.");
+
+                output.close();
+                input.close();
+                connection.close();
+
+            }
+
+        }catch(Exception e){
+            System.out.println("Could not connect to server");
+        }
+    }
+}
