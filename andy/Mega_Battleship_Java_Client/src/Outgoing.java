3c83c31d8371511981248444ecc4609490520fe5
Andrew Lewis <ajlewis1@me.com>
2018-11-19 14:36:16 -0700

    Updated Incoming and Outgoing classes.

--- andy/Mega_Battleship_Java_Client/src/Outgoing.java
+++ andy/Mega_Battleship_Java_Client/src/Outgoing.java
@@ -2,48 +2,35 @@ public class Outgoing {
 
     private int boardLength = 20;
     private int boardWidth = 20;
-    private int[][] _playerGameBoard = new int [boardLength][boardWidth];
-    private int[][] _opponentGameBoard = new int [boardLength][boardWidth];
-    private boolean _gameOver;
-    private boolean _winLose;
-    private boolean _hitMiss;
+    private int[][] _playerGameBoard = new int[boardLength][boardWidth];
+    private int[][] _opponentGameBoard = new int[boardLength][boardWidth];
+    private int playerScore;
+    private int opponentScore;
 
-    public void setGameBoards(int[][]playerGameBoard, int[][]opponentGameBoard){
+    public void setGameBoards(int[][] playerGameBoard, int[][] opponentGameBoard) {
 
         _playerGameBoard = playerGameBoard;
         _opponentGameBoard = opponentGameBoard;
 
     }
 
-    public int[][] getPlayerGameBoard(){
+    public int[][] getPlayerGameBoard() {
         return _playerGameBoard;
     }
 
-    public int[][] getOpponentGameBoard(){
-        return _opponentGameBoard;
+    public void setPlayerScore(int playerScore) {
+        this.playerScore = playerScore;
     }
 
-    public void set_gameOver(boolean gameOver) {
-        _gameOver = gameOver;
+    public int getPlayerScore() {
+        return playerScore;
     }
 
-    public boolean get_gameOver(){
-        return _gameOver;
+    public void setOpponentScore(int opponentScore) {
+        this.opponentScore = opponentScore;
     }
 
-    public void set_winLose(boolean winLose) {
-        _winLose = winLose;
-    }
-
-    public boolean getWinLose(){
-        return _winLose;
-    }
-
-    public void set_hitMiss(boolean hitMiss) {
-        _hitMiss = hitMiss;
-    }
-
-    public boolean get_hitMiss(){
-        return _hitMiss;
+    public int getOpponentScore() {
+        return opponentScore;
     }
 }
